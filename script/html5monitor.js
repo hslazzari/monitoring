2f27562782ac6d2242848e112940c0e07b850337
Hugo Lazzari <hugoschroterl@gmail.com>
2015-07-27 13:00:47 -0300

    Adicionado questionário e configurações.
    Adicionado questionário e configurações.

--- script/html5monitor.js
+++ script/html5monitor.js
@@ -99,17 +99,37 @@ function start_monitor() {
 			    	});
 			}
 		
+				var fa = document.createElement('style');
+				fa.type = 'text/css';
+				fa.textContent = '@font-face { font-family: FontAwesome; src: url("'
+				+ chrome.extension.getURL('/fonts/glyphicons-halflings-regular.woff')
+				+ '"); }';
 
-			/*
+				var fa2 = document.createElement('style');
+				fa2.type = 'text/css';
+				fa2.textContent = '@font-face { font-family: FontAwesome; src: url("'
+				+ chrome.extension.getURL('/fonts/glyphicons-halflings-regular.ttf')
+				+ '"); }';
+
+
+				document.head.appendChild(fa);
+				
+				document.head.appendChild(fa2);
+				
+
+
+			
 	    		BootstrapDialog.show({
-		            message: 'Como foi sua experiência durante a exibição do vídeo?',
+	    			title: "Qualidade de experiência",
+		            //message: 'Como foi sua experiência durante a exibição do vídeo?<form class="form-horizontal" role="form"><div class="form-group"><label class="control-label col-sm-2" for="email">Email:</label><div class="col-sm-10"><input type="email" class="form-control" id="email" placeholder="Enter email"></div></div><div class="form-group"><label class="control-label col-sm-2" for="pwd">Password:</label><div class="col-sm-10"><input type="password" class="form-control" id="pwd" placeholder="Enter password"></div></div><div class="form-group"><div class="col-sm-offset-2 col-sm-10"><div class="checkbox"><label><input type="checkbox"> Remember me</label></div></div></div><div class="form-group"><div class="col-sm-offset-2 col-sm-10"><button type="submit" class="btn btn-default">Submit</button></div></div></form>',
+		            message: $('<div></div>').load(chrome.extension.getURL("remote.html")),
 		            buttons: [{
 		            	label: 'Boa',
 		                cssClass: 'btn-primary',
 		                action: function(){
 		                    //alert('Hi Orange!');
-		                    saveFile = saveFile + "Opinion,Good" + '\n';
-		                    $link.attr("href", 'data:Application/octet-stream,' + encodeURIComponent(saveFile))[0].click();
+		                   // saveFile = saveFile + "Opinion,Good" + '\n';
+		                   // $link.attr("href", 'data:Application/octet-stream,' + encodeURIComponent(saveFile))[0].click();
 			
 		                }
 		            }, {
@@ -117,18 +137,18 @@ function start_monitor() {
 		                cssClass: 'btn-danger',
 		                action: function(){
 		                    //alert('Hi Orange!');
-		                    saveFile = saveFile + "Opinion,Ruim" + '\n';
-		                    $link.attr("href", 'data:Application/octet-stream,' + encodeURIComponent(saveFile))[0].click();
+		                   // saveFile = saveFile + "Opinion,Ruim" + '\n';
+		                   // $link.attr("href", 'data:Application/octet-stream,' + encodeURIComponent(saveFile))[0].click();
 			
 		                }
 		            }, {
-		                label: 'Close',
+		                label: 'Cancelar',
 		                action: function(dialogItself){
 		                    dialogItself.close();
 		                }
 		            }]
 		        });
-			*/
+			
 
 		});
 
